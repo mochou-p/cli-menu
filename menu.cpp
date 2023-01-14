bfb459889487539c68391e192650a65aef282ad0
vrecusko <vrecusko20@gmail.com>
2023-01-14 10:56:49 +0100

    string input parsing into linked list structure

--- menu.cpp
+++ menu.cpp
@@ -1,1 +1,34 @@
 //
+
+#include "menu.hpp"
+
+MenuNode::MenuNode(std::string _str)
+{
+    std::size_t pos = _str.find(",");
+
+    text = _str.substr(0, pos);
+
+    if (pos < _str.length())
+        next = new MenuNode(_str.substr(pos + 1));
+}
+
+MenuNode::~MenuNode()
+{
+    delete next;
+}
+
+Menu::Menu(std::string _str)
+{
+    data = new MenuNode(_str);
+}
+
+Menu::~Menu()
+{
+    delete data;
+}
+
+void Menu::render()
+{
+    for (MenuNode* p = data; p != nullptr; p = p->next)
+        std::cout << p->text.c_str() << std::endl;
+}
--- menu.hpp
+++ menu.hpp
@@ -1,1 +1,34 @@
 //
+
+#pragma once
+
+#ifndef __menu_hpp_
+#define __menu_hpp_
+
+#include <iostream>
+#include <string>
+
+class MenuNode
+{
+public:
+
+    MenuNode(std::string);
+    ~MenuNode();
+
+    std::string text;
+    MenuNode*   next = nullptr;
+};
+
+class Menu
+{
+public:
+    Menu(std::string);
+    ~Menu();
+
+    void render();
+
+private:
+    MenuNode* data = nullptr;
+};
+
+#endif  // __menu_hpp_
